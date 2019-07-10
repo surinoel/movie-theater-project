e3330025d737d56358b863a4e95bf9ff1ac39962
surinoel <48958763+surinoel@users.noreply.github.com>
2019-07-10 20:50:10 +0900

    Add files via upload

--- ArduinoPR/DEV1/DEV1.ino
+++ ArduinoPR/DEV1/DEV1.ino
@@ -1,28 +1,107 @@
-int pinPos = 5, pinNeg = 4, pinEn = 3;
-int pwmSpeed = 100;
+int exFlag = 0;
+int pinPos = 4, pinNeg = 2, pinEn = 3;
+int tmpSpeed, pwmSpeed = 100;
+int prevKeyValue, curKeyValue;
+const int sw_row[4] = {13, 12, 11, 10};
+const int sw_col[4] = {9, 8, 7, 6};
+const int sw_gnd = 5;
+char buf[30];
+
+int getPWMSpeed(const int);
+void incsMtSpeed(void);
+void descMtSpeed(void);
+void serialWriteCall(int);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  for (int i = 0; i < 4; i++) {
+    pinMode(sw_row[i], OUTPUT);
+    pinMode(sw_col[i], INPUT);
+  }
+  pinMode(sw_gnd, OUTPUT);
+  digitalWrite(sw_gnd, LOW);
+
   pinMode(pinPos, OUTPUT); pinMode(pinNeg, OUTPUT); pinMode(pinEn, OUTPUT);
+  digitalWrite(pinPos, HIGH);
+  digitalWrite(pinNeg, LOW);
   analogWrite(pinEn, pwmSpeed);
 
+  memset(buf, 0, sizeof(buf));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  curKeyValue = -1;
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(sw_row[i], HIGH);
+    for (int j = 0; j < 4; j++) {
+      if ((i == 2 && j == 3) || (i == 3 && j == 0) || (i == 3) && (j == 2)) continue;
+      if (digitalRead(sw_col[j])) {
+        delay(100);
+        if(prevKeyValue != i*4 + j) {
+          curKeyValue = i*4 + j;  
+        }
+        break;
+      }
+    }
+    digitalWrite(sw_row[i], LOW);
+  }
+  if(curKeyValue != -1) {
+    tmpSpeed = getPWMSpeed(curKeyValue);
+    if(tmpSpeed != -1) {
+      if(exFlag)
+      { 
+        if(pwmSpeed >= 0 && pwmSpeed <= 255) {
+          analogWrite(pinEn, pwmSpeed);
+          serialWriteCall(pwmSpeed);  
+        }
+        pwmSpeed = 0;
+        exFlag = 0;
+      }
+      else {
+        pwmSpeed = pwmSpeed * 10 + tmpSpeed;    
+      }    
+    }
+    Serial.println(tmpSpeed);  
+  }
+  prevKeyValue = curKeyValue;
 }
 
-void serialEvent(void) 
+void incsMtSpeed() {
+  pwmSpeed = (pwmSpeed + 10 > 255) ? 0 : pwmSpeed += 10;
+  analogWrite(pinEn, pwmSpeed);
+  serialWriteCall(pwmSpeed);
+}
+
+void descMtSpeed() {
+  pwmSpeed = (pwmSpeed - 10 <= 0) ? 0 : pwmSpeed -= 10;
+  analogWrite(pinEn, pwmSpeed);
+  serialWriteCall(pwmSpeed);
+}
+
+int getPWMSpeed(const int keyValue) {
+  switch(keyValue) {
+    case 3: incsMtSpeed(); return -1;
+    case 7: descMtSpeed(); return -1;
+    case 0: return 7;
+    case 1: return 8;
+    case 2: return 9;
+    case 4: return 4;
+    case 5: return 5;
+    case 6: return 6;
+    case 8: return 1;
+    case 9: return 2;
+    case 10: return 3;
+    case 13: return 0;
+    case 15: exFlag = 1;
+  }
+}
+
+void serialWriteCall(int pwmSpeed)
 {
-  char data;
-  data = Serial.read();
-  if(data == 'o') {
-    digitalWrite(12, 0);
-  }
-  else if(data == 'x') {
-    digitalWrite(12, 1); 
-  }
-  Serial.write(data);
+  String msg = "PWM,"; 
+  msg += pwmSpeed; 
+  msg += "\n";
+  msg.toCharArray(buf, 30);
+  Serial.write(buf);
+  memset(buf, 0, sizeof(buf));
 }
--- ArduinoPR/DEV3/DEV3.ino
+++ ArduinoPR/DEV3/DEV3.ino
@@ -1,50 +1,82 @@
-#include <DHT.h>
-#define DHTTYPE DHT11
-
-String sCommand = "";
-int pinDht = 2;
-char buf[30], cTemp;
+#include <LiquidCrystal.h>
+                                         
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8, 7, 6, A0, A1, A2);
+String sCommand = "", sStart, sTmp;
+char cTemp;
+String Temp, Humi; 
+int receiveFlag = 0;
 int pinPos = 5, pinNeg = 4, pinEn = 3;
 int pwmSpeed = 100;
+char pwmSpeedBuf[30];
 
-DHT dht(pinDht, DHTTYPE);
+byte user_value[8]={
+    B00010,
+    B00101,
+    B00010,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000
+};
 
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
   pinMode(pinPos, OUTPUT); pinMode(pinNeg, OUTPUT); pinMode(pinEn, OUTPUT);
+  digitalWrite(pinPos, HIGH);
+  digitalWrite(pinNeg, LOW);
   analogWrite(pinEn, pwmSpeed);
-  dht.begin();
+  lcd.begin(16, 2);
+  lcd.createChar(0, user_value);
+  lcd.clear();
 }
 
 void loop() {
-  delay(300);
-  float fTemp = dht.readTemperature();
-  float fHumi = dht.readHumidity();
-  
-  if (isnan(fTemp) || isnan(fHumi)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+  int ret;
+  if(receiveFlag) {
+    sStart = "", sTmp = "";
+    ret = sCommand.indexOf(',');
+    if(ret != -1) {
+      sTmp = sCommand.substring(0, ret);
+      sCommand = sCommand.substring(ret+1, sCommand.length());
+      if(sTmp == "TH") {
+          ret = sCommand.indexOf(',');
+          Temp = sCommand.substring(0, ret);                
+          sCommand = sCommand.substring(ret+1, sCommand.length());
+          Humi = sCommand.substring(0, sCommand.length());
+          lcd.setCursor(0, 0);
+          String buf = Humi + "% " + Temp;
+          lcd.print(buf);  
+          lcd.write(static_cast<byte>(0x00));
+          lcd.print('C');
+      }
+      else if(sTmp == "PWM") {
+          ret = sCommand.indexOf(',');
+          sTmp = sCommand.substring(0, ret);
+          sTmp.toCharArray(pwmSpeedBuf, 30);
+          pwmSpeed = atoi(pwmSpeedBuf);                        
+          lcd.setCursor(0, 1);
+          String buf = "PWM "; buf += pwmSpeed; buf += "%";
+          lcd.print(buf);
+          analogWrite(pinEn, pwmSpeed);
+      }
+    }
+    
+    receiveFlag = 0; 
+    sCommand = "";
   }
 
+}
+
+void serialEvent(void) 
+{
   while(Serial.available())
   {
      cTemp = Serial.read();
      if(cTemp == '\n') {  // 개행까지 받는다
-        sCommand += '\n'; 
-        sCommand.toCharArray(buf, 30);
-        Serial.write(buf);
-        memset(buf, 0, sizeof(buf));
-        sCommand = "";
-        break;     
+        receiveFlag = 1;
+        return;
      }
      sCommand += cTemp;
   }
-
-  String msg1 = "TH,"; 
-  msg1 += (int)fTemp; 
-  msg1 += ",";
-  msg1 += (int)fHumi;
-  msg1 += "\n";
-  msg1.toCharArray(buf, 30);
-  Serial.write(buf);
 }
